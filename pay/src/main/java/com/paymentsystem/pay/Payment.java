11233a0e551bd1dd948248e34c8c5768fe55f53a
Kat Bassett <87139530+katbassett@users.noreply.github.com>
2025-05-23 16:39:28 -0500

    Add files via upload

---
+++ pay/src/main/java/com/paymentsystem/pay/Payment.java
@@ -0,0 +1,53 @@
+package com.paymentsystem.pay;
+
+public class Payment {
+    private String cardNumber;
+    private String cardHolderName;
+    private String expirationDate;
+    private String cvv;
+    private String amount;
+
+    public Payment(){
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public String getCardHolderName() {
+        return cardHolderName;
+    }
+
+    public void setCardHolderName(String cardHolderName) {
+        this.cardHolderName = cardHolderName;
+    }
+
+    public String getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(String expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    public String getCvv() {
+        return cvv;
+    }
+
+    public void setCvv(String cvv) {
+        this.cvv = cvv;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+    
+}
---
+++ pay/src/main/java/com/paymentsystem/pay/PaymentController.java
@@ -0,0 +1,31 @@
+package com.paymentsystem.pay;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+
+import org.springframework.http.ResponseEntity;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+@RestController
+public class PaymentController {
+
+    @GetMapping("/")
+    public void redirectToPaymentPage(HttpServletResponse response) throws IOException {
+        response.sendRedirect("/payment.html");
+}
+
+    @PostMapping("/make-payment")
+    public ResponseEntity<String> handlePayment(@ModelAttribute Payment payment, HttpServletResponse response) throws IOException {
+        double amount = Double.parseDouble(payment.getAmount());
+
+        if (amount > 1000) {
+            response.sendRedirect("/declined.html");
+            return ResponseEntity.status(302).header("Location", "/declined.html").body("Redirecting to declined page");
+        } else {
+            response.sendRedirect("/confirmation.html");
+            return ResponseEntity.status(302).header("Location", "/confirmation.html").body("Redirecting to confirmation page");
+        }
+    }
+}
